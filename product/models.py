40c595b22b5db6ec8b78ddc1305e8ca764bbaed9
Amin <asaa47721@gmail.com>
2020-02-01 00:05:43 +0330

    define Item model. makemigrations

--- product/models.py
+++ product/models.py
@@ -12,3 +12,22 @@ class Category(models.Model):
     class Meta:
         ordering = ['position']
         verbose_name_plural = 'Categories'
+
+
+
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+    discount_price = models.FloatField(blank=True, null=True)
+    category = models.ForeignKey(Category, blank=True, null=True,
+        on_delete=models.SET_NULL)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    image = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['-created_at']
