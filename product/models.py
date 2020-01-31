a27a498b92d74a39a7e3722de8cb428af18fa312
Amin <asaa47721@gmail.com>
2020-01-31 23:46:09 +0330

    add product app to install apps. define Category model. makemigrations

--- product/models.py
+++ product/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    label = models.CharField(max_length=100)
+    position = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['position']
