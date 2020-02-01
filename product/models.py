47b01f6ca40f559ea3e80a28256b6ad368f1c019
Amin <asaa47721@gmail.com>
2020-02-01 18:36:22 +0330

    define get_absolute_url for Item model

--- product/models.py
+++ product/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.shortcuts import reverse
 
 
 class Category(models.Model):
@@ -29,5 +30,8 @@ class Item(models.Model):
     def __str__(self):
         return self.title
     
+    def get_absolute_url(self):
+        return reverse('product:detail', kwargs={'slug': self.slug})
+
     class Meta:
         ordering = ['-created_at']
