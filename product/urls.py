42c896734e97f814a965ce4a8b6e51594da514d2
Amin <asaa47721@gmail.com>
2020-02-01 12:04:43 +0330

    define urls and tmeplates for ItemListView and ItemDetailView

---
+++ product/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'core'
+
+urlpatterns = [
+    # item list
+    path('', views.ItemListView.as_view(), name='list'),
+
+    # item detail
+    path('product/<slug>/', views.ItemDetailView.as_view(), name='detail'),
+]
