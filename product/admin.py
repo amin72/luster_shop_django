6c9c26abe53e93c85c70e6ebda0b2d30ae052405
Amin <asaa47721@gmail.com>
2020-02-04 02:10:02 +0330

    add Order, OrderItem, Address, Payment to admin

--- product/admin.py
+++ product/admin.py
@@ -2,8 +2,16 @@ from django.contrib import admin
 from .models import (
     Category,
     Item,
+    OrderItem,
+    Order,
+    Address,
+    Payment,
 )
 
 
 admin.site.register(Category)
 admin.site.register(Item)
+admin.site.register(OrderItem)
+admin.site.register(Order)
+admin.site.register(Address)
+admin.site.register(Payment)
