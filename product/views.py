8a23dadeade4b1a2b45fd7facb3be3b1f9227b54
Amin <asaa47721@gmail.com>
2020-02-04 02:10:54 +0330

    define add_to_cart view. add it to urls

--- product/views.py
+++ product/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, View
+from django.contrib import messages
+from django.utils import timezone
 
 from .models import (
     Item,
+    OrderItem,
+    Order,
 )
 
 
@@ -13,3 +17,37 @@ class ItemListView(ListView):
 
 class ItemDetailView(DetailView):
     model = Item
+
+
+
+def add_to_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_item, created = OrderItem.objects.get_or_create(
+        item=item,
+        user=request.user,
+        ordered=False)
+
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item in the order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+            messages.info(request, "This item quantity was updated.")
+            return redirect('product:list')
+        else:
+            messages.info(request, "This item was added to your cart.")
+            order_item.quantity = 1
+            order_item.save()
+            order.items.add(order_item)
+            return redirect('product:list')
+    else:
+        # order does not exist. create order and add order item to it.
+        ordered_date = timezone.now()
+        order = Order.objects.create(user=request.user,
+            ordered_date=ordered_date)
+        order.items.add(order_item)
+        messages.info(request, "This item was added to your cart.")
+        return redirect('product:list')
