f2ffcca740582d3ae4765fcb4608ecc6eba77f08
Amin <asaa47721@gmail.com>
2020-02-07 01:21:22 +0330

    define signup view

--- profile/views.py
+++ profile/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.utils.translation import ugettext_lazy as _
+from django.core.exceptions import ValidationError
+from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+from .forms import SignUpForm
+
+
+
+def signup(request):
+    form = SignUpForm(request.POST or None)
+
+    if form.is_valid():
+        user = form.save(commit=False)
+        phone_number = form.cleaned_data.get('phone_number')
+        password = form.cleaned_data.get('password')
+
+        try:
+            validate_password(password, user)
+            user.set_password(password)
+            # password field is validated, now save form
+            form.save()
+
+            # authenticate and login user
+            new_user = authenticate(phone_number=phone_number,
+                password=password)
+            login(request, new_user)
+
+            messages.success(request, 'User created successfully!')
+            return redirect('product:list')
+        except ValidationError as e:
+            form.add_error('password', e)
+
+    context = {
+        'form': form
+    }
+    return render(request, "account/signup.html", context)
