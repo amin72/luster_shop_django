0fa5e645a073e2094b6849241b5e4d68d2d3091f
Amin <asaa47721@gmail.com>
2020-02-07 01:17:24 +0330

    define UserManager. set objects=UserManager

--- profile/models.py
+++ profile/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
@@ -18,6 +18,31 @@ def phone_number_validator(value):
 
 
 
+class UserManager(BaseUserManager):
+    def create_user(self, phone_number, email, password, is_superuser=False,
+        is_staff=False, is_active=False):
+
+        if not phone_number:
+            raise ValueError("I KNOW YOU HAVE A NAME")
+        if not password:
+            raise ValueError("PASSWORD?!?!?!? HELLO??")
+
+        user = self.model(email=self.normalize_email(email))
+        user.phone_number = phone_number
+        user.set_password(password)
+        user.is_superuser = is_superuser
+        user.is_staff = is_staff
+        user.is_active = is_active
+        user.save()
+        return user
+
+    def create_superuser(self, phone_number, email, password):
+        user = self.create_user(phone_number, email, password,
+            is_superuser=True, is_staff=True, is_active=True)
+        return user
+
+
+
 class CustomUser(AbstractUser):
     phone_number = models.CharField(max_length=11,
         unique=True, verbose_name=_('Phone Number'),
@@ -29,7 +54,10 @@ class CustomUser(AbstractUser):
         blank=True,
         null=True)
 
+
     USERNAME_FIELD = 'phone_number'
+    objects = UserManager()
+
 
     class Meta:
         unique_together = ('phone_number', 'username', 'email')
